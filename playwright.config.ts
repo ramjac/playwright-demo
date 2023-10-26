d19e5c2826dde8d94cafe229a052869f86e5e23b
Grady Fields <gfields@captechconsulting.com>
2023-10-25 20:41:58 -0400

    Added the first working playwright test

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
 
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://127.0.0.1:8080',
 
     // Collect trace when retrying the failed test.
     trace: 'on-first-retry',
@@ -34,9 +34,9 @@ export default defineConfig({
     },
   ],
   // Run your local dev server before starting the tests.
-  webServer: {
-    command: 'npm run start',
-    url: 'http://127.0.0.1:3000',
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
--- tests/firsttest.spec.ts
+++ tests/firsttest.spec.ts
@@ -1,7 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const name = await page.innerText('.navbar__title');
-  expect(name).toBe('Playwright');
+  await page.goto('/');
+
+  // Page defaults to say "Playwright"
+  await expect(page.getByRole('heading', { name: 'Playwright' })).toBeVisible();
+
+  // Fill and submit the form
+  await page.getByLabel('Name').fill('Test 1');
+  await page.getByLabel('Description').fill('This is a test!');
+  await page.getByRole('button').dispatchEvent('click');
+
+  // Check the the page is updated based on the form
+  await expect(page.getByRole('heading', { name: 'Test 1' })).toBeVisible();
 });
